4d2704014ba5d7a48a05aa7bb5f644c37cd5b45d
Dodo9531 <oge2782@mai.ru>
2023-12-04 15:24:44 +0300

    Added database initialization and deleted driver from properties

--- src/main/java/ru/kata/spring/boot_security/demo/models/Role.java
+++ src/main/java/ru/kata/spring/boot_security/demo/models/Role.java
@@ -1,9 +1,6 @@
 package ru.kata.spring.boot_security.demo.models;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 import org.springframework.security.core.GrantedAuthority;
 
 import javax.persistence.*;
@@ -23,5 +20,8 @@ public class Role implements GrantedAuthority {
     @Override
     public String getAuthority() {
         return name;
+    }
+    public Role (String rolename) {
+        name = rolename;
     }
 }
